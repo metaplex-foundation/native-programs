d19f79173760f3c8eb4290b98dcb076fd370a53e
Blockiosaurus <blockiosaurus@gmail.com>
2024-07-30 16:37:14 -0400

    Trimming validator progams

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -9,20 +9,5 @@ function getProgram(programBinary) {
 module.exports = {
   validator: {
     commitment: "processed",
-    programs: [
-      // Below are external programs that should be included in the local validator.
-      // You may configure which ones to fetch from the cluster when building
-      // programs within the `configs/program-scripts/dump.sh` script.
-      {
-        label: "Token Metadata",
-        programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-        deployPath: getProgram("mpl_token_metadata.so"),
-      },
-      {
-        label: "SPL Noop",
-        programId: "noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV",
-        deployPath: getProgram("spl_noop.so"),
-      },
-    ],
   },
 };
